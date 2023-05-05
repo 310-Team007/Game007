8e790f08a5308e05c45ef16fac85d476cda1e0a0
Spencer Wigren <77368960+spencerwigren@users.noreply.github.com>
2023-05-05 09:03:53 -0600

    importing files tutorial

---
+++ Python_Tutorial/importing_file.py
@@ -0,0 +1,18 @@
+"""
+This file will go over how to import a file
+and use it code
+"""
+
+import tutorial as tt
+
+
+def main():
+    # setting code from tutorial file
+    code = tt
+    
+    # calling main
+    code.main()
+
+
+if __name__ == "__main__":
+  main()
