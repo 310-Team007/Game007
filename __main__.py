b1db984db5c4653a29764eb3ebcc852020f6d1e9
Spencer Wigren <77368960+spencerwigren@users.noreply.github.com>
2023-05-12 09:05:31 -0600

    Start Background movement by tracking the player

--- __main__.py
+++ __main__.py
@@ -2,27 +2,41 @@ import pygame
 
 import constants as c
 import display
-
+import player
 
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
-    show = display.MovingBackground()
+    # show = display.MovingBackground()
+    player_sprite = player.Player((0, 255, 255), 5, 10)
     
+    
+    # see pygame tutural
+    player_pos = pygame.Vector2(c.SCREEN_WIDTH / 2, c.SCREEN_HEIGHT / 2)
+    clock_speed = clock.tick(c.FPS) / 10000
+    
+    # draws player
+
     running = True
-    
     while running:
-        
         clock.tick(c.FPS)
         
-        show.move_background()
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
+                running = False        
+        
+        
+        # drwas, gets movement and apply movement
+  
+        movement = pygame.key.get_pressed()
+        player_sprite.move(player_pos, clock_speed, movement)
+        
+        
+        
                 
         pygame.display.update()
 
 if __name__ == "__main__":
-    main()  
+    main()
