be690c82b845228fbdd925e8c195a52470a1b066
Spencer Wigren <77368960+spencerwigren@users.noreply.github.com>
2023-06-30 09:45:15 -0600

    spritesheets size

--- enemy.py
+++ enemy.py
@@ -18,3 +18,13 @@ class enemy(Player):
     spritesheet = SpriteSheet(enemy_image_grunt)
     spritesheet = SpriteSheet(enemy_image_sniper)
     spritesheet = SpriteSheet(enemy_image_boss)
+    
+    # Grunts:
+    # 1234 x 1080px
+    
+    # Sniper:
+    # 2944 x 1536px
+    
+    # Boss:
+    # 2432 x 1280px
+  
