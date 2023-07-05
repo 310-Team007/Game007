37aded0f8993c77d6a1e3bcf7f2b5516ce004c3d
Kaleb Schauerhamer <97472668+kbsch77@users.noreply.github.com>
2023-07-05 09:29:48 -0600

    added some work and possible measurements

--- enemy.py
+++ enemy.py
@@ -15,23 +15,34 @@ class enemy(Player):
     enemy_image_sniper = pygame.image.load('images/craftpix-soldiers/PSD/Soldier_3_Spritelist.psd').convert_alpha()
     enemy_image_boss = pygame.image.load('images/craftpix-soldiers/PSD/Soldier_2_Spritelist.psd').convert_alpha()
 
+
+# common enemy
+def grunt():
+    enemy()
     # Grunts: 1234 x 1080px
-    # frame size:
-    spritesheet_grunt = SpriteSheet(enemy_image_grunt)
+    # frame size: 126 x 126px ?
+    spritesheet_grunt = animate.SpriteSheet(enemy.enemy_image_grunt)
 
+    DrawEnemy()
+
+# uncommon, sneaky enemy
+def sniper():
+    enemy()
     # Sniper: 2944 x 1536px
-    # frame size:
-    spritesheet_sniper = SpriteSheet(enemy_image_sniper)
-    
+    # frame size: 126 x 126px ? (Dosen't start on row 0)
+    spritesheet_sniper = animate.SpriteSheet(enemy.enemy_image_sniper)
+
+    DrawEnemy()
+
+# uncommon, big enemy
+def boss():
+    enemy()
     # Boss: 2432 x 1280px
-    # frame size:
-    spritesheet_boss = SpriteSheet(enemy_image_boss)
+    # frame size: 126 x 126px ?
+    spritesheet_boss = animate.SpriteSheet(enemy.enemy_image_boss)  
 
-    def grunt():
-        pass
+    DrawEnemy()
 
-    def sniper():
-        pass
 
-    def boss():
-        pass    
+def DrawEnemy(self, player_pos, frame, image_amount, row_number):
+    animate.ShowSprite(self, player_pos, frame, image_amount, row_number)
