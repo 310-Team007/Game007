adba5e275513a32ef7cee49a66db3b9f7613b3a6
jegunderson <jegunderson@byui.edu>
2023-07-14 13:54:54 -0600

    change bullet height and enemy collision

--- bullet.py
+++ bullet.py
@@ -6,7 +6,7 @@ class Bullets(pygame.sprite.Sprite):
     def __init__(self, origin, width, height): 
         pygame.init()
                
-        self.bullet = pygame.Rect(origin.x, origin.y -  c.PLAYER_HEIGHT/2 , width, height)
+        self.bullet = pygame.Rect(origin.x, origin.y -  (c.PLAYER_HEIGHT * c.PLAYER_SCALE) / 2 , width, height)
         self.bullet_alive = False
         
         
@@ -16,11 +16,12 @@ class Bullets(pygame.sprite.Sprite):
         return bullet
     
     def bullet_collide(self, character_pos, player_pos):
-        if (self.bullet.left <= character_pos.x + (c.PLAYER_WIDTH) and self.bullet.left + c.BULLET_WIDTH >= character_pos.x and self.bullet.top <= character_pos.y + c.PLAYER_HEIGHT * c.PLAYER_SCALE and self.bullet.top + c.BULLET_HEIGHT >= character_pos.y - c.PLAYER_HEIGHT):
+        if (self.bullet.left <= character_pos.x + (c.PLAYER_WIDTH) and self.bullet.left + c.BULLET_WIDTH >= character_pos.x and self.bullet.top >= character_pos.y - c.PLAYER_HEIGHT * c.PLAYER_SCALE and self.bullet.top + c.BULLET_HEIGHT <= character_pos.y):
             self.set_dead(player_pos)
+            print("Hit")
         if self.bullet.left == c.SCREEN_WIDTH - 300:
             self.set_dead(player_pos)
-        
+            print("Miss")
         
     def set_alive(self):
         self.bullet_alive = True
@@ -28,7 +29,7 @@ class Bullets(pygame.sprite.Sprite):
     def set_dead(self, player_pos):
         self.bullet_alive = False
         self.bullet.left = player_pos.x
-        self.bullet.top = player_pos.y
+        self.bullet.top = player_pos.y - (c.PLAYER_HEIGHT * c.PLAYER_SCALE) / 2
 
         
     
